9a0544a42b17aa88a90eca8aad86eb953f5af104
Jdob11 <jbdobson11@gmail.com>
2024-04-08 16:25:26 -0700

    chore: Change column names for compass requirements

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -6,7 +6,7 @@ DROP TABLE IF EXISTS property_reviews CASCADE;
 CREATE TABLE users(
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
-  email_address VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
   password VARCHAR(255) NOT NULL
 );
 
@@ -26,8 +26,8 @@ CREATE TABLE properties(
   street VARCHAR(255) NOT NULL,
   city VARCHAR(255) NOT NULL,
   province VARCHAR(255) NOT NULL,
-  postal_code VARCHAR(255) NOT NULL,
-  is_active BOOLEAN NOT NULL DEFAULT true
+  post_code VARCHAR(255) NOT NULL,
+  active BOOLEAN NOT NULL DEFAULT true
 );
 
 
