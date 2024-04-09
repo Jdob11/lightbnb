87a0c660f1d719717c0bdff314d719e58589dcb0
Jdob11 <jbdobson11@gmail.com>
2024-04-09 13:32:46 -0700

    feat: Add initial reservations to the reservations table

--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -1,9 +1,9 @@
--- INSERT INTO users (name, email, password)
--- VALUES
--- ('Eva Stanley', 'sebastianguerra@ymail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
--- ('Louisa Meyer', 'jacksonrose@hotmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
--- ('Dominic Parks' ,'victoriablackwell@outlook.com' , '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.')
--- ;
+INSERT INTO users (name, email, password)
+VALUES
+('Eva Stanley', 'sebastianguerra@ymail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+('Louisa Meyer', 'jacksonrose@hotmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+('Dominic Parks' ,'victoriablackwell@outlook.com' , '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.')
+;
 
 INSERT INTO properties (
 owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code, active
@@ -14,13 +14,14 @@ VALUES
 (2, 'Habit mix', 'description', 'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg', 46058, 0, 5, 6, 'Canada', '1650 Hejto Center', 'Genwezuj', 'Newfoundland And Labrador', 44583, true)
 ;
 
--- reservations
-
--- `start_date` |  `end_date`  | `property_id` | `guest_id`
--- --------------|--------------|---------------|----------
--- 2018-09-11   | 2018-09-26   |             2 |        3
--- 2019-01-04   | 2019-02-01   |             2 |        2
--- 2023-10-01   | 2023-10-14   |             1 |        3
+INSERT INTO reservations(
+start_date, end_date, property_id, guest_id
+)
+VALUES
+('2019-01-04', '2019-02-01', 2, 2),
+('2018-09-11', '2018-09-26', 2, 3),
+('2023-10-01', '2023-10-14', 1, 3)
+;
 
 -- property_reviews
 
