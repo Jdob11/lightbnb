ccc9ef328d8cacb466a85c6755ee032931e980a9
Jdob11 <jbdobson11@gmail.com>
2024-04-15 17:35:20 -0700

    refactor: Change getAllReservations to query database

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -32,6 +32,7 @@ const getUserWithEmail = (email) => {
     })
     .catch((err) => {
       console.log(err.message);
+      return Promise.reject(err);
     });
 };
 
@@ -55,6 +56,7 @@ const getUserWithId = function (id) {
   })
   .catch((err) => {
     console.log(err.message);
+    return Promise.reject(err);
   });
 };
 
@@ -80,6 +82,7 @@ const addUser = function (user) {
   })
   .catch((err) => {
     console.log(err.message);
+    return Promise.reject(err);
   });
 };
 
@@ -93,9 +96,30 @@ const addUser = function (user) {
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function (guest_id, limit = 10) {
-  return getAllProperties(null, 2);
-};
-
+  
+  return pool
+  .query(
+    `
+    SELECT  reservations.*,
+    properties.*
+    FROM reservations
+    JOIN properties ON reservations.property_id = properties.id
+    JOIN property_reviews ON properties.id = property_reviews.property_id
+    WHERE reservations.guest_id = $1
+    GROUP BY properties.id, reservations.id
+    ORDER BY reservations.start_date
+    LIMIT $2;
+    `,
+    [guest_id, limit])
+    .then((result) => {
+      return result.rows.length > 0 ? result.rows : null;
+    })
+    .catch((err) => {
+      console.log(err.message);
+      return Promise.reject(err);
+    });
+  };
+    
 /// Properties
 
 /**
