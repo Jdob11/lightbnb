dc1a3f5fe2e59a67a9abd40c0331189b868b414a
Jdob11 <jbdobson11@gmail.com>
2024-04-15 17:41:15 -0700

    refactor: Update pool.query in getAllReservations function to include average rating and only pull reservations that haven't passed

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -101,11 +101,13 @@ const getAllReservations = function (guest_id, limit = 10) {
   .query(
     `
     SELECT  reservations.*,
-    properties.*
+    properties.*,
+    AVG(property_reviews.rating) as average_rating
     FROM reservations
     JOIN properties ON reservations.property_id = properties.id
     JOIN property_reviews ON properties.id = property_reviews.property_id
     WHERE reservations.guest_id = $1
+    AND reservations.end_date < now()::date
     GROUP BY properties.id, reservations.id
     ORDER BY reservations.start_date
     LIMIT $2;
